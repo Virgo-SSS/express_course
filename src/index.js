72e667e4314f854d99f904e82c559993882fc939
virgo <virgostevanus27@gmail.com>
2025-03-09 16:29:40 +0700

    static file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import express from "express";
 import {router} from "./routes/user.js";
 import { ErrorMiddleware } from "./middleware/errorMiddleware.js";
+import path from 'path';
+import { fileURLToPath } from "url";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const staticPath = path.join(__dirname, '../static');
+console.log(staticPath);
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.static(staticPath));
+app.use('/file',express.static(staticPath)); // contoh untuk mendapatkan static file tapi menggunkan url path /file contoh: http://localhost:3000/file/hallo.txt
+
 app.use('/user', router);
 
 app.use(ErrorMiddleware); // ini harus di letakan di paling akhir
